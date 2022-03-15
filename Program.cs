0b9907a0051a37e0cff39853d08eafef2e678fa3
DiogenesTSO <51807077+DiogenesTSO@users.noreply.github.com>
2022-03-15 20:39:39 -0300

    Add files via upload
    Iniciando aprendizado em C#, irei postando cada avanço desse progresso;

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+
+static void Main(String[] args){
+
+byte idade = 24;
+int codigo = 2323;
+char genero =  'M';
+
+double preco1 = 10000;
+double preco2 = 20000;
+
+double x = 16;
+Math.Sqrt(16);
+System.Console.WriteLine(x);
+
+
+System.Console.WriteLine("O preço do computador é: " +preco1);
+System.Console.WriteLine("Mesa do escritorio custa: " +preco2);
+System.Console.WriteLine("Registro:" +codigo+ "\nIdade:"+idade+ "\nGenero:" +genero);
+
+}
+}
